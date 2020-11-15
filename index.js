0bff151d17e7fe94e744016651e7f1d314822c1d
Sandeep Gupta <sandeepguptabc@gmail.com>
2020-11-15 13:21:23 +0530

    Helmet Added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const musics = require("./routes/musics");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
 const app = express();
-
-
 app.use(express.json());
+app.use(helmet());
+app.use(morgan("tiny"));
 app.use("/api", musics);
 
 app.listen(4800, () => console.log("Connected to port 4800") )
